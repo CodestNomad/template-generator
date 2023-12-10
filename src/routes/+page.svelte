1feee473d4a7331be599bbc4d1f0b0d21452802f
davediv <33143202+davediv@users.noreply.github.com>
2023-12-10 20:31:49 +0700

    Install Sveltekit, TailwindCSS, and DaisyUI

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<main class="container mx-auto px-4">
+    <article>
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+        <button class="btn btn-primary">Hello Yo!</button>
+        <button class="btn btn-secondary">Hello Yo!</button>
+        <button class="btn btn-info">Hello Yo!</button>
+        <button class="btn btn-danger">Hello Yo!</button>
+
+    </article>
+
+</main>
---
+++ src/routes/theme1/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    // export let data;
+</script>
+
+<header class="container mx-auto p-3 flex justify-center items-center">
+    <img src="https://www.refactoringui.com/_next/static/media/book.5380927448c9872170bbc9fc9e5828c4.png" alt="NextJs">
+</header>
+
+<main class="container mx-auto p-3 text-center">
+    <article>
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+        <button class="btn btn-primary">Hello Yo!</button>
+        <button class="btn btn-secondary">Hello Yo!</button>
+        <button class="btn btn-info">Hello Yo!</button>
+        <button class="btn btn-danger">Hello Yo!</button>
+        <hr>
+        <a href="https://www.google.com/" class="btn btn-primary">Hello Yo!</a>
+    </article>
+</main>
+
+<footer>
+    <p>Footer</p>
+</footer>
